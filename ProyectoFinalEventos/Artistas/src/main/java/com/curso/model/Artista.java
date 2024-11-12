3727459c45ca0aabc853f0b9429d58ee8c8a4854
Victor Tercero <M.Victortercero@gmail.com>
2024-11-12 13:34:41 +0100

    Cambios comentados en la defensa del codigo

--- ProyectoFinalEventos/Artistas/src/main/java/com/curso/model/Artista.java
+++ ProyectoFinalEventos/Artistas/src/main/java/com/curso/model/Artista.java
@@ -1,7 +1,11 @@
 package com.curso.model;
 
-import jakarta.persistence.*;
-import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  * Representa un artista en el sistema.
@@ -39,7 +43,6 @@ public class Artista {
      * Indica si el artista está disponible para reservas o eventos.
      */
     @Column(name = "disponible", nullable = false)
-    @Schema(description = "Disponibilidad del artista para reservas o eventos", example = "true", required = true)
     private Boolean disponible;
 
     // Getters y Setters
